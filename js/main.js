87561a267d0a727dc70acbb09fa600d179e0d1a3
sccau189 <sccau189@hotmail.com>
2020-06-21 16:26:45 +0800

    AOS及Slick完成

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-$("#banner").slick();
+AOS.init();
+
+$("#banner").slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+});
+
+$("#brand").slick({
+    slidesToShow:4,
+    autoplay:true,
+    autoplaySpeed:1000,
+    dots: true,
+    arrows: false,
+});
